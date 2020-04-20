dfa2a0328a8a78786d3b195a073f591e78c8541d
Jasmine Rivera <jasmineannrivera@gmail.com>
2020-04-20 17:18:06 -0500

    end of day commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,4 +1,10 @@
 
+import com.google.common.base.Ascii;
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
@@ -14,6 +20,14 @@ public class Test {
             System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
             System.out.println("Reversed: " + StringUtils.reverse(userInput));
         }
+
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(420).height(20);
+        builder.element(new PseudoText(userInput));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
 
 
 
